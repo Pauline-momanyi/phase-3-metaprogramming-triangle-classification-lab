025c1438c8580a7764a6c415cfbe24d282edd427
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-06-11 01:20:56 +0300

    commit tree abd11ab3bcc22bfddd669c408363f02a72dbb008

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
   # write code here
+  attr_reader :a, :b, :c
+  def initialize(a, b, c)
+    @a = a
+    @b = b 
+    @c = c
+  end
+
+  def kind 
+    # [a, b, c].all?(&:positive?)
+    if a > 0 && b > 0 && c > 0 && a+b>c && a+c>b && b+c>a
+      if (a == b) && (b == c)
+        :equilateral
+      elsif (a == b) || (a == c) || (b == c)
+        :isosceles
+      elsif (a != b) && (b != c)
+        :scalene
+      end    
+    else
+      raise TriangleError
+    end
+  
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
